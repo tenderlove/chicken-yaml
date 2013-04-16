0e2bae46e464e5fd453034a26151659169129699
Aaron Patterson <aaron.patterson@gmail.com>
2013-04-16 11:48:42 -0700

    refactoring tests

--- tests/run.scm
+++ tests/run.scm
@@ -11,34 +11,36 @@
               (lambda (alias seed) (append seed (list (list 'alias alias))))
               '()))
 
+(define (find-event event-name)
+  (lambda (event) (eq? event-name (car event))))
+
 (test-group "stream"
   (test "start" 'stream-start (caar (yaml-exp "--- foo"))))
 
 (test-group "document-start"
   (test "version" (list 'document-start '(1 1) '())
-                    (find (lambda (event) (eq? 'document-start (car event)))
+                    (find (find-event 'document-start)
                           (yaml-exp "%YAML 1.1\n--- foo")))
   (test "no version" (list 'document-start '() '())
-                    (find (lambda (event) (eq? 'document-start (car event)))
+                    (find (find-event 'document-start)
                           (yaml-exp "--- foo")))
   (test "tags" (list 'document-start '() (list (cons "!" "tag:tenderlovemaking.com,2009:")))
-                    (find (lambda (event) (eq? 'document-start (car event)))
+                    (find (find-event 'document-start)
                           (yaml-exp "%TAG ! tag:tenderlovemaking.com,2009:\n--- foo")))
 )
 
 (test-group "document-end"
   (test "implicit" '(document-end #t)
-                  (find (lambda (event) (eq? 'document-end (car event)))
+                    (find (find-event 'document-end)
                         (yaml-exp "--- foo")))
   (test "explicit" '(document-end #f)
-                  (find (lambda (event) (eq? 'document-end (car event)))
+                    (find (find-event 'document-end)
                         (yaml-exp "--- foo\n...")))
 )
 
 (test-group "alias"
-  (test "A" '(alias "A")
-            (find (lambda (event) (eq? 'alias (car event)))
-                  (yaml-exp "---\n- &A foo\n- *A"))))
+  (test "A" '(alias "A") (find (find-event 'alias)
+                               (yaml-exp "---\n- &A foo\n- *A"))))
 
 (test-end)
 (test-exit)
