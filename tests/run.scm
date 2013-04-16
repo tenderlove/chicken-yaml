40badb215045cfd3f6dc96b8c1bd0618f1c0591d
Aaron Patterson <aaron.patterson@gmail.com>
2013-04-16 11:50:47 -0700

    more test refactoring

--- tests/run.scm
+++ tests/run.scm
@@ -11,36 +11,33 @@
               (lambda (alias seed) (append seed (list (list 'alias alias))))
               '()))
 
-(define (find-event event-name)
-  (lambda (event) (eq? event-name (car event))))
+(define (find-event event-name events)
+  (find (lambda (event) (eq? event-name (car event))) events))
 
 (test-group "stream"
   (test "start" 'stream-start (caar (yaml-exp "--- foo"))))
 
 (test-group "document-start"
   (test "version" (list 'document-start '(1 1) '())
-                    (find (find-event 'document-start)
+                    (find-event 'document-start
                           (yaml-exp "%YAML 1.1\n--- foo")))
   (test "no version" (list 'document-start '() '())
-                    (find (find-event 'document-start)
+                    (find-event 'document-start
                           (yaml-exp "--- foo")))
   (test "tags" (list 'document-start '() (list (cons "!" "tag:tenderlovemaking.com,2009:")))
-                    (find (find-event 'document-start)
+                    (find-event 'document-start
                           (yaml-exp "%TAG ! tag:tenderlovemaking.com,2009:\n--- foo")))
 )
 
 (test-group "document-end"
   (test "implicit" '(document-end #t)
-                    (find (find-event 'document-end)
-                        (yaml-exp "--- foo")))
+                    (find-event 'document-end (yaml-exp "--- foo")))
   (test "explicit" '(document-end #f)
-                    (find (find-event 'document-end)
-                        (yaml-exp "--- foo\n...")))
+                    (find-event 'document-end (yaml-exp "--- foo\n...")))
 )
 
 (test-group "alias"
-  (test "A" '(alias "A") (find (find-event 'alias)
-                               (yaml-exp "---\n- &A foo\n- *A"))))
+  (test "A" '(alias "A") (find-event 'alias (yaml-exp "---\n- &A foo\n- *A"))))
 
 (test-end)
 (test-exit)
